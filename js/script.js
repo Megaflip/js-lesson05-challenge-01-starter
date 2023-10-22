db3e82f0d1a28846f8964c7250e1260e5044c18e
Megaflip <megaflip@gmail.com>
2023-10-22 09:40:45 -0400

    6th commit

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@ var cat = document.querySelector(".cat");
 // console.log(cat);
 
 button.addEventListener("click", function () {
-    if (cat.classList.remove(".show-cat")) { 
+    // button.innerText = "It's working";
+    // cat.classList.add("show");
+    if (cat.classList.contains("show")) { 
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
         //console.log("Yes");
       } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
         //console.log("No");
       }
-    });
+});
